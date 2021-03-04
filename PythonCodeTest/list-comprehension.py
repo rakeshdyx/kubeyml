698f89405297d8b5ff33042ced4f72b2ceaa59ad
rakeshdyx <rakeshdyx@gmail.com>
2021-03-04 07:17:59 +0530

    updated on 04032021

--- PythonCodeTest/list-comprehension.py
+++ PythonCodeTest/list-comprehension.py
@@ -1,5 +1,22 @@
+"""
 a = [x for x in range(10)]
 print(a)
 
+##from a string
+h_letter = [letter for letter in 'human']
+print(h_letter)
+
+##if condition
 d = [x for x in range(10) if x%2==1]
 print(d)
+
+
+##with if-else
+obj = ["Even" if x % 2 == 0 else "Odd" for x in range(10)]
+print(obj)
+
+### Nested if for list comprehension
+print([y for y in range(100) if y % 2 == 0 if y % 5 == 0])
+"""
+### Nested loop for list comprehension
+
---
+++ PythonCodeTest/tuples.py
@@ -0,0 +1,18 @@
+""""""
+
+import hashlib
+t = input().split()
+#print(t)
+#print(hash(tuple(t)))
+
+#int_list = str(intiget_map)
+tu[] = 0
+for i in range(len(t)):
+    tu[].append(i[i], ' ')
+
+print(hash(t_t))
+
+
+
+
+
