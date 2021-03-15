1753c85cdde65f6bcba26cb2c751c6698f0d283a
rakeshdyx <rakeshdyx@gmail.com>
2021-03-15 06:08:34 +0530

    150320121

--- PythonCodeTest/list-comprehension.py
+++ PythonCodeTest/list-comprehension.py
@@ -17,6 +17,34 @@ print(obj)
 
 ### Nested if for list comprehension
 print([y for y in range(100) if y % 2 == 0 if y % 5 == 0])
+
+### Nested loop for list comprehension with Transpose of Matrix using Nested Loops
+
+##Transpose of Matrix using Nested Loops
+
+transposed = []
+matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+
+for i in range(len(matrix[0])):
+    transposed_row = []
+
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+print(transposed)
+
+##Using nested list comprehension
+matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+transposed_matrix = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+print(transposed_matrix)
+
 """
-### Nested loop for list comprehension
+##using multiple parallal for loops
+
+x, y, z, n = [int(input()) for _ in range(4)]
+#print(x, y, z, n)
+#print all possible coordinates in i,k,j format with constriant 0<=i<= x, 0<=j<= y, 0<=k<= z and remove the cordinate if i+j+k=n
+cordinates_list = [[a, b, c] for a in range(0, x+1) for b in range(0, y+1) for c in range(0, z+1) if a + b +c != n]
+print(cordinates_list)
+
 
